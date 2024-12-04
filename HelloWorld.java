1326fcd7b0ebede346c7acfb5858859b32ee47ac
SANGAMESH S  Havinal <32675692+sanagamesh@users.noreply.github.com>
2024-12-04 12:16:58 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,5 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("WELCOME TO GITHUB");
-    System.out.println("WELCOME TO GIT");
   }
 }
